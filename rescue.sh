b5ba0de65774b730783d0052deee9dbb0dad3680
Hayk <haikhakobyann@gmail.com>
2026-02-25 18:34:43 +0400

    Second homework fixed

--- rescue.sh
+++ rescue.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt install -y nginx 
+apt update && apt upgrade -y
+apt install -y nginx 
 cd /var/www/html/ 
 
 
 for page in page1.html page2.html page3.html; do
- sudo touch $page
- sudo chown www-data:www-data $page
- sudo chmod 644 $page
+touch $page
+chown www-data:www-data $page
+chmod 644 $page
  done
 
-if ( sudo systemctl is-active --quiet apache2 ) then
+if ( systemctl is-active --quiet nginx ) then
  echo "Nginx is running. Force restarting now..." 
- sudo systemctl restart nginx
+systemctl restart nginx
  else
  echo "Nginx is dead. Starting it now..."
- sudo systemctl start nginx
+systemctl start nginx
 fi
- sudo journalctl -u nginx -n 5
+journalctl -u nginx -n 5
